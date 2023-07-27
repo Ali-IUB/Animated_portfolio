82089093ceb7f7e88ba3225d2e5a1b38fbfa3699
Ali-IUB <114250003+Ali-IUB@users.noreply.github.com>
2023-07-27 22:13:10 +0500

    Add files via upload

---
+++ assests/js/main.js
@@ -0,0 +1,19 @@
+AOS.init({
+    offset:120,
+    delay:0,
+    duration:700,
+    easing:'ease',
+    once:false,
+    mirror:false,
+    anchorPlacement:'top-bottom'
+})
+
+// fancyBox Bind module
+
+fancyBox.bind('[data-fancyBox]')
+const options = {
+    Thumbs: {
+      type: "modern",
+    },
+  };
+  
